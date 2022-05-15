c640a7ddf545399dedd143d5d8d1690cb97b842f
levchenkoyarya <levchenkoyarya@gmail.com>
2022-05-15 21:17:06 +0300

    login view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Request, Get, Post, Render, UseGuards } from "@nestjs/common";
+import { Controller, Request, Get, Post, Render, UseGuards, Header } from "@nestjs/common";
 import { AuthService } from "./auth/auth.service";
 import { LocalAuthGuard } from "./auth/local-auth.guard";
 import { JwtAuthGuard } from "./auth/jwt-auth.guard";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private appService: AppService) {
+  }
+
+  @Get("auth/login")
+  @Render("login")
+  index() {
+    return { message: "Login page" };
   }
 
   @UseGuards(LocalAuthGuard)
