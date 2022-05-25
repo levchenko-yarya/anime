6de25e0129ddbfb0a80f2bf052b052c49991fc2c
levchenkoyarya <levchenkoyarya@gmail.com>
2022-05-25 17:48:30 +0300

    fix movie-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MovieModule } from "./movie/movie.module";
-import { DatabaseModule } from "./database/database.module";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
   imports: [MongooseModule.forRoot("mongodb://localhost/anime"),
-    AuthModule, UsersModule],
+    AuthModule, UsersModule, MovieModule],
   controllers: [AppController],
   providers: [AppService]
 })
